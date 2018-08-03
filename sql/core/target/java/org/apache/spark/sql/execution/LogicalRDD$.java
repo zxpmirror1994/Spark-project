58525f92757c1f936cb1b45811229275d228380b
Xiaopei Zhang <zxpmirror1994@gmail.com>
2018-08-03 16:17:12 -0700

    Upload Spark Project

---
+++ sql/core/target/java/org/apache/spark/sql/execution/LogicalRDD$.java
@@ -0,0 +1,10 @@
+package org.apache.spark.sql.execution;
+// no position
+// not preceding
+public  class LogicalRDD$ implements scala.Serializable {
+  /**
+   * Static reference to the singleton instance of this Scala object.
+   */
+  public static final LogicalRDD$ MODULE$ = null;
+  public   LogicalRDD$ () { throw new RuntimeException(); }
+}
---
+++ sql/core/target/java/org/apache/spark/sql/parquet/ParquetTableScan$.java
@@ -0,0 +1,10 @@
+package org.apache.spark.sql.parquet;
+// no position
+// not preceding
+public  class ParquetTableScan$ extends scala.runtime.AbstractFunction3<scala.collection.Seq<org.apache.spark.sql.catalyst.expressions.Attribute>, org.apache.spark.sql.parquet.ParquetRelation, scala.collection.Seq<org.apache.spark.sql.catalyst.expressions.Expression>, org.apache.spark.sql.parquet.ParquetTableScan> implements scala.Serializable {
+  /**
+   * Static reference to the singleton instance of this Scala object.
+   */
+  public static final ParquetTableScan$ MODULE$ = null;
+  public   ParquetTableScan$ () { throw new RuntimeException(); }
+}
